f8b94e8cd4b1b20c22d03281a199a52b5de83cfb
OctopusLian <zoctopus@qq.com>
2023-07-23 12:56:05 +0800

    feat: packet实现

--- packet/packet.go
+++ packet/packet.go
@@ -1,5 +1,10 @@
 package packet
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // Packet协议定义
 
 /*
@@ -26,3 +31,90 @@ const (
 	CommandConnAck   = iota + 0x80 // 0x81, 连接请求的响应包
 	CommandSubmitAck               //0x82, 消息请求的响应包
 )
+
+type Packet interface {
+	Decode([]byte) error     // []byte -> struct
+	Encode() ([]byte, error) //  struct -> []byte
+}
+
+type Submit struct {
+	ID      string
+	Payload []byte
+}
+
+func (s *Submit) Decode(pktBody []byte) error {
+	s.ID = string(pktBody[:8])
+	s.Payload = pktBody[8:]
+	return nil
+}
+
+func (s *Submit) Encode() ([]byte, error) {
+	return bytes.Join([][]byte{[]byte(s.ID[:8]), s.Payload}, nil), nil
+}
+
+type SubmitAck struct {
+	ID     string
+	Result uint8
+}
+
+func (s *SubmitAck) Decode(pktBody []byte) error {
+	s.ID = string(pktBody[0:8])
+	s.Result = uint8(pktBody[8])
+	return nil
+}
+
+func (s *SubmitAck) Encode() ([]byte, error) {
+	return bytes.Join([][]byte{[]byte(s.ID[:8]), []byte{s.Result}}, nil), nil
+}
+
+func Decode(packet []byte) (Packet, error) {
+	commandID := packet[0]
+	pktBody := packet[1:]
+
+	switch commandID {
+	case CommandConn:
+		return nil, nil
+	case CommandConnAck:
+		return nil, nil
+	case CommandSubmit:
+		s := Submit{}
+		err := s.Decode(pktBody)
+		if err != nil {
+			return nil, err
+		}
+		return &s, nil
+	case CommandSubmitAck:
+		s := SubmitAck{}
+		err := s.Decode(pktBody)
+		if err != nil {
+			return nil, err
+		}
+		return &s, nil
+	default:
+		return nil, fmt.Errorf("unknown commandID [%d]", commandID)
+	}
+}
+
+func Encode(p Packet) ([]byte, error) {
+	var commandID uint8
+	var pktBody []byte
+	var err error
+
+	switch t := p.(type) {
+	case *Submit:
+		commandID = CommandSubmit
+		pktBody, err = p.Encode()
+		if err != nil {
+			return nil, err
+		}
+	case *SubmitAck:
+		commandID = CommandSubmitAck
+		pktBody, err = p.Encode()
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("unknown type [%s]", t)
+	}
+	return bytes.Join([][]byte{[]byte{commandID}, pktBody}, nil), nil
+}
